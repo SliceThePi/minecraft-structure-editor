c55c39a86b61d12b24ed5da944969206e412bb42
SliceThePi <slicethepi@gmail.com>
2018-06-11 18:15:08 +0000

    Fixed loading multiple structures with multiple palettes

--- structure-editor.js
+++ structure-editor.js
@@ -108,7 +108,7 @@ module.exports = {
 
             for (let i = 0; i < firstPalettes.length; i++)
                 for (let k = 0; k < secondPalettes.length; k++)
-                    outputPalettes.push(firstPalettes[i].concat(secondPalettes[k]));
+                    outputPalettes.push(firstPalettes[i].value.concat(secondPalettes[k]));
 
             output.value["palettes"].value.value = outputPalettes;
         }
